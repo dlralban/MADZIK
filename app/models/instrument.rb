278f6da1819a00834d3328d785f252de8150d85c
Julien Nguyen <nguyenjulien93@gmail.com>
2022-11-21 16:33:56 +0100

    Back end works

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,5 +2,5 @@ class Instrument < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :address, presence: true, length: { minimum: 6 }
-  validates price:, presence: true
+  validates :price, presence: true
 end
