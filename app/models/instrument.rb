599f3afe13395dfc48f82a546f154b9aafda3284
Julien Nguyen <nguyenjulien93@gmail.com>
2022-11-21 17:34:34 +0100

    Back end works finally

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,5 +1,6 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+  has_many :rentals, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true, length: { minimum: 6 }
   validates :price, presence: true
