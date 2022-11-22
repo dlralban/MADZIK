363fa5d3f70aa5c87c1e28e8f9c7a5020093a647
Alban de La Rochefordiere <dlralban@gmail.com>
2022-11-22 11:50:45 +0100

    Pundit

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,18 +1,34 @@
 class RentalsController < ApplicationController
   before_action :set_instrument, only: %i[create]
+  before_action :set_rental, only: %i[edit update show]
 
   def new
     @rental = Rental.new
+    authorize @rental # Add this line
   end
 
   def create
     @rental = Rental.new(rental_params)
+    authorize @rental
     if @rental.valid?
       redirect_to instruments_path(@instrument)
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    authorize @rental
+  end
+
+  def update
+    authorize @rental
+  end
+
+  def index
+    @rentals = policy_scope(Rental)
+  end
+
 
   private
 
@@ -21,6 +37,11 @@ class RentalsController < ApplicationController
   end
 
   def set_instrument
-    @instrument = Instrument.find(params[:id])
+    @instrument = Instrument.find(params[:instrument_id])
   end
+
+  def set_rental
+    @rental = Rental.find(params[:id])
+  end
+
 end
