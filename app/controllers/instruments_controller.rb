9b1260727465461e4b6aa3c36009f61687ce12cb
sebastien cuthbert <blueton23@gmail.com>
2022-11-23 13:11:26 +0100

    View my instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,7 +2,8 @@ class InstrumentsController < ApplicationController
   before_action :set_instrument, only: %i[show edit update destroy]
 
   def index
-    @instruments = policy_scope(Instrument)
+    @my_instruments = policy_scope(Instrument)
+    @instruments = Instrument.all.where.not(user: current_user)
   end
 
   def show
